114e8d4673711f23f057b6d8756f42ce53ff7d6f
Guem <guem@gmx.de>
2009-07-26 12:55:12 +0200

    load google maps key depending on environment

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -2,5 +2,4 @@
 # Return the configurations defined in ./config/config.yml
 #
 raw_config = File.read(RAILS_ROOT + "/config/config.yml")
-#APP_CONFIG = YAML.load(raw_config)[RAILS_ENV]
-APP_CONFIG = YAML.load(raw_config)[Socket.gethostname]
+APP_CONFIG = YAML.load(raw_config)[RAILS_ENV]
